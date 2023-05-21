f5146a8933180584e394fbd8547d3f811eec696e
Iyad Ahmed <iyadahmed430@gmail.com>
2023-05-21 21:58:46 +0300

    Parallelize rendering using OpenMP

--- render_mesh.c
+++ render_mesh.c
@@ -25,7 +25,17 @@ int main(int argc, char **argv)
     Triangle *triangles = NULL;
     uint32_t triangle_count = 0;
     BVH *bvh_ptr = NULL;
+    uint8_t *image = NULL;
     Millis_Timer timer;
+
+    int width = 1200;
+    int height = 800;
+
+    image = malloc(width * height * 3 * sizeof(uint8_t));
+    if (image == NULL)
+    {
+        goto cleanup;
+    }
 
     timer_start(&timer);
     load_binary_stl(mesh_filepath, &triangles, &triangle_count);
@@ -43,10 +53,6 @@ int main(int argc, char **argv)
         goto cleanup;
     }
 
-    int width = 1200;
-    int height = 800;
-    uint8_t color[3] = {255, 0, 255};
-
     fprintf(ppm_file, "P6\n%d %d\n255\n", width, height);
 
     Vec3 camera_location = VEC3(50, -200, 50);
@@ -54,7 +60,12 @@ int main(int argc, char **argv)
     Vec3 camera_right = VEC3(1, 0, 0);
     Vec3 camera_forward = VEC3(0, 2, 0);
 
-    for (int j = 0; j < height; j++)
+    timer_start(&timer);
+
+    // NOTE: we declare j before the OpenMP pragma to make it work with MSVC
+    int j;
+#pragma omp parallel for schedule(dynamic)
+    for (j = 0; j < height; j++)
     {
         for (int i = 0; i < width; i++)
         {
@@ -97,20 +108,22 @@ int main(int argc, char **argv)
 
             if (is_hit)
             {
-                color[0] = map_float_to_zero_one(min_t) * 255;
-                color[1] = 0;
-                color[2] = 0;
+                image[(i + j * width) * 3 + 0] = map_float_to_zero_one(min_t) * 255;
+                image[(i + j * width) * 3 + 1] = 0;
+                image[(i + j * width) * 3 + 2] = 0;
             }
             else
             {
-                color[0] = 255;
-                color[1] = 255;
-                color[2] = 255;
+                image[(i + j * width) * 3 + 0] = 255;
+                image[(i + j * width) * 3 + 1] = 255;
+                image[(i + j * width) * 3 + 2] = 255;
             }
-
-            fwrite(color, sizeof(uint8_t), 3, ppm_file);
         }
     }
+
+    printf("Rendering took %lu ms\n", timer_stop(&timer));
+
+    fwrite(image, sizeof(uint8_t), width * height * 3, ppm_file);
 
 cleanup:
     if (ppm_file)
@@ -121,6 +134,9 @@ cleanup:
 
     if (bvh_ptr)
         free_bvh(bvh_ptr);
+
+    if (image)
+        free(image);
 
     return 0;
 }
